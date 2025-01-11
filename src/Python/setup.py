f232b43db984d507c478634340cf0a9148b349a0
Anawaert <anawaertmain@outlook.com>
2025-01-11 16:15:42 +0800

    修改setup.py

--- src/Python/setup.py
+++ src/Python/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(name='unireo',
-      version='0.1.0',
+      version='0.1.2',
       description='A universal SDK for synchronized dual-eye cameras compliant with the UVC protocol',
       long_description=long_description,
       long_description_content_type="text/markdown",
